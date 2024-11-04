2551e9a88dff74ae53d7d3be2397254b29a1082c
iamjumshaid <iamjumshaidkhan@gmail.com>
2024-11-04 22:02:41 +0100

    🎉 Intialise exercises

---
+++ dl2024-ex02-mlps-dl2024/lib/plot.py
@@ -0,0 +1,18 @@
+"""ReLU plot."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib.activations import ReLU
+
+
+def plot_relu() -> None:
+    """Plot the ReLU function in the range (-4, 4).
+
+    Returns:
+        None
+    """
+    # START TODO #################
+    # Create input data, run through ReLU and plot.
+    raise NotImplementedError
+    # END TODO###################
